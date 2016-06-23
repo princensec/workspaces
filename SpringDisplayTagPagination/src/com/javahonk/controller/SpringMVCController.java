c2dcc420ce2d8536843dd73af89db81b067b9ddd
prasenjeet kumar <princensec@gmil.com>
2016-06-23 21:28:17 +0530

    updated

--- SpringDisplayTagPagination/src/com/javahonk/controller/SpringMVCController.java
+++ SpringDisplayTagPagination/src/com/javahonk/controller/SpringMVCController.java
@@ -16,28 +16,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SpringMVCController {
 
-    @RequestMapping(value = "/paginationDisplayTag.web", method = 
-            RequestMethod.GET)
-    public String printWelcome(@ModelAttribute("person") 
-    Person person,BindingResult result, ModelMap model, 
-    HttpServletRequest request,
-            HttpServletResponse response) {
+	@RequestMapping(value = "/paginationDisplayTag.web", method = RequestMethod.GET)
+	public String printWelcome(@ModelAttribute("person") Person person, BindingResult result, ModelMap model,
+			HttpServletRequest request, HttpServletResponse response) {
 
-	List<Person> personsList = new ArrayList<Person>();
-        for (int i = 0; i < 223; i++) {
-            Person person2 = new Person();
-            person2.setName("JohnLandy"+i);
-            person2.setPosition("System Architect"+i);
-            person2.setSalary("$320,800");
-            person2.setLocation("NY"+(i-1));
-            person2.setPhone("999999999");
-            personsList.add(person2);  
+		System.out.println("check");
+		List<Person> personsList = new ArrayList<Person>();
+		for (int i = 0; i < 223; i++) {
+			Person person2 = new Person();
+			person2.setName("JohnLandy" + i);
+			person2.setPosition("System Architect" + i);
+			person2.setSalary("$320,800");
+			person2.setLocation("NY" + (i - 1));
+			person2.setPhone("999999999");
+			personsList.add(person2);
+		}
+
+		person.setPersonList(personsList);
+		return "paginationDisplayTag";
+
 	}
-        
-        person.setPersonList(personsList);
-        return "paginationDisplayTag";
 
-    }
-
- 
 }
